dc90f710a4e6a0b9d663df8f8c90e77d2241631a
Wim van Arkel <warkel@me.com>
2018-03-02 21:52:19 +0100

    Initial Commit

---
+++ Window Shopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Window Shopper
+//
+//  Created by W.S. van Arkel jr. on 02-03-18.
+//  Copyright © 2018 DutchAppFactory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Window ShopperTests/Window_ShopperTests.swift
@@ -0,0 +1,36 @@
+//
+//  Window_ShopperTests.swift
+//  Window ShopperTests
+//
+//  Created by W.S. van Arkel jr. on 02-03-18.
+//  Copyright © 2018 DutchAppFactory. All rights reserved.
+//
+
+import XCTest
+@testable import Window_Shopper
+
+class Window_ShopperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
