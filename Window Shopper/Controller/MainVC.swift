ed13d159398f4667e18def0381a03ca1aea6e3f8
Wim van Arkel <warkel@me.com>
2018-03-03 13:40:39 +0100

    UI finished and text working with keyboard

--- Window Shopper/Controller/MainVC.swift
+++ Window Shopper/Controller/MainVC.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTxt: CurrencyTextField!
+    @IBOutlet weak var itemPriceTxt: CurrencyTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.caculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        itemPriceTxt.inputAccessoryView = calcBtn
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func caculate() {
+        
     }
-
-
 }
 
